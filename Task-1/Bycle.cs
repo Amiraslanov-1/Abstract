b4f60677852c3608507a92c40e43c02353ac10ac
Amiraslanov-1 <99805882+Amiraslanov-1@users.noreply.github.com>
2022-03-22 03:49:54 +0400

    task ended

---
+++ Task-1/Bycle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task18march
+{
+    class Bycle : Vehicle
+    {
+        public override void Drive(double km)
+        {
+            Millage += km;
+        }
+        public override void ShowInfo()
+        { 
+            base.ShowInfo();
+            Console.WriteLine("Class Bycle");
+        }
+    }
+}
